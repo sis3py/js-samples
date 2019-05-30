767faee9ba9958fefb921423508a24fd053f6430
sis3py <romainmx@gmail.com>
2019-05-30 10:51:24 +0200

    + Update transparent-caching

--- transparent-caching.js
+++ transparent-caching.js
@@ -69,3 +69,115 @@ console.timeEnd("heavy() no cache");
 console.time("heavy() cache");
 heavy(20);
 console.timeEnd("heavy() cache");
+
+// Spy decorator
+// Keep track of all the calls made to a given function
+
+function spy(func) {
+  function wrapper(...args) {
+    wrapper.calls.push(args);
+    return func.apply(this, arguments);
+  }
+
+  wrapper.calls = [];
+
+  return wrapper;
+}
+
+function sum(a, b) {
+  return a + b;
+}
+
+sum = spy(sum);
+sum(1, 2);
+sum(3, 4);
+sum(5, 6);
+console.log(sum.calls); // [[1, 2], [3, 4], [5, 6]]
+
+// Delaying decorator
+// Delays each call of a function by n milliseconds
+
+function delay(func, n) {
+  return function() {
+    setTimeout(() => func.apply(this, arguments), n);
+  };
+}
+
+function thinkBeforeTalk(thoughtOut) {
+  console.log(thoughtOut);
+}
+
+thinkBeforeTalk = delay(thinkBeforeTalk, 1000);
+thinkBeforeTalk("Brilliant idea"); // Brilliant idea, after 1 second
+
+// Debounce decorator
+// Guarantees that all other future calls of a function
+// in the closest n milliseconds will be ignored.
+
+function debounce(func, n) {
+  let isCooldown = false;
+
+  return function() {
+    if (isCooldown) return;
+
+    func.apply(this, arguments);
+
+    isCooldown = true;
+
+    setTimeout(() => (isCooldown = false), n);
+  };
+}
+
+function dontSpamMe(message) {
+  console.log(message);
+}
+
+dontSpamMe = debounce(dontSpamMe, 2000);
+dontSpamMe("Hello"); // Hello
+dontSpamMe("How are you ?"); // Nothing
+dontSpamMe("Are you fine ?"); // Nothing
+dontSpamMe("Please answer me !"); // Nothing
+dontSpamMe("Ok goodbye"); // Nothing
+
+// Throttle decorator
+// idem debounce but if an ignored call is the last during the cooldown,
+//  then it executes at the end of the delay.
+function throttle(func, ms) {
+  let isThrottled = false;
+  let savedArgs;
+  let savedThis;
+
+  function wrapper() {
+    if (isThrottled) {
+      // If is currently throttled, store the context / args
+      savedArgs = arguments;
+      savedThis = this;
+      return;
+    }
+
+    func.apply(this, arguments); // First call
+
+    isThrottled = true;
+
+    setTimeout(function() {
+      isThrottled = false; // No longer throttled
+      if (savedArgs) {
+        wrapper.apply(savedThis, savedArgs); // Call the wrapper with the saved context / args
+        savedArgs = null;
+        savedThis = null;
+      }
+    }, ms);
+  }
+
+  return wrapper;
+}
+
+function dontSpamMeButAtLeastSayGoodbye(message) {
+  console.log(message);
+}
+
+dontSpamMeButAtLeastSayGoodbye = throttle(dontSpamMeButAtLeastSayGoodbye, 2000);
+dontSpamMeButAtLeastSayGoodbye("Hello"); // Hello
+dontSpamMeButAtLeastSayGoodbye("How are you ?"); // Nothing
+dontSpamMeButAtLeastSayGoodbye("Are you fine ?"); // Nothing
+dontSpamMeButAtLeastSayGoodbye("Ok goodbye"); // Ok goodbye
